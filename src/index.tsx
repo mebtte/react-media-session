199e834128d9bbe0cc444a48f984043c060f0f2d
mebtte <hi@mebtte.com>
2020-09-15 15:41:26 +0800

    fix fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { HAS_MEDIA_SESSION, MediaSessionProps } from './constant';
 import useMediaSession from './use_media_session';
 
 import MediaSession from './media_session';
 
-const MediaSessionWrapper = (
-  props: React.PropsWithChildren<MediaSessionProps>,
-) => {
+const MediaSessionWrapper = (props: React.PropsWithChildren<MediaSessionProps>) => {
   if (!HAS_MEDIA_SESSION) {
-    return <>{props.children || null}</>;
+    return <Fragment>{props.children || null}</Fragment>;
   }
   return <MediaSession {...props} />;
 };
--- src/media_session.tsx
+++ src/media_session.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { MediaSessionProps } from './constant';
 import useMediaSession from './use_media_session';
@@ -6,7 +6,7 @@ import useMediaSession from './use_media_session';
 const MediaSession = (props: React.PropsWithChildren<MediaSessionProps>) => {
   const { children, ...rest } = props;
   useMediaSession(rest);
-  return <>{children || null}</>;
+  return <Fragment>{children || null}</Fragment>;
 };
 
 export default MediaSession;
